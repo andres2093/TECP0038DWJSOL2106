8af7c9ff38142751636952e4b1f36d72b4e4e4fa
Andres Ramirez <andres200493@gmail.com>
2021-11-20 13:35:27 -0600

    S2 - R1

--- s2/server.js
+++ s2/server.js
@@ -32,21 +32,55 @@ const fs = require('fs')
 //   }
 // )
 
-fs.writeFileSync(
-  path.join(__dirname, '/ejemplo_1.txt'), 
-  'Este es el contenido de mi archivo', 
-  { encoding: 'utf-8' }
-)
-console.log('Archivo creado!!!');
+// fs.writeFileSync(
+//   path.join(__dirname, '/ejemplo_1.txt'), 
+//   'Este es el contenido de mi archivo', 
+//   { encoding: 'utf-8' }
+// )
+// console.log('Archivo creado!!!');
 
+// let data = fs.readFileSync(
+//   path.join(__dirname, '/ejemplo_1.txt'), 
+//   { encoding: 'utf-8' }
+// )
+// console.log('Lectura: ', data);
+
+// fs.appendFileSync(
+//   path.join(__dirname, '/ejemplo_1.txt'), 
+//   '\nEsto es una nueva línea',
+// )
+// console.log('Archivo actualizado!!!');
+
+// Reto 1
 let data = fs.readFileSync(
-  path.join(__dirname, '/ejemplo_1.txt'), 
-  { encoding: 'utf-8' }
+  path.join(__dirname, '/input.txt'), 
+  // { encoding: 'utf-8' }
 )
-console.log('Lectura: ', data);
+console.log(data);
 
-fs.appendFileSync(
-  path.join(__dirname, '/ejemplo_1.txt'), 
-  '\nEsto es una nueva línea',
-)
-console.log('Archivo actualizado!!!');
+let lines = 0, spaces = 0, caracter = 0
+
+data.map(c => {
+  if(c === 10){
+    lines++
+  } else if(c === 32){
+    spaces++
+  } else {
+    caracter++
+  }
+})
+
+// for(i in data){
+//   if(data.charCodeAt(i) === 10){
+//     lines++
+//   } else if(data.charCodeAt(i) === 32){
+//     spaces++
+//   } else {
+//     caracter++
+//   }
+// }
+
+console.log("Caracteres: " + data.length);
+console.log("Caracteres: " + caracter);
+console.log("Lineas: " + lines);
+console.log("Espacios: " + spaces);
