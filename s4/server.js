6708755831109fc1675d0a57f5b9b1f4866916e7
Andres Ramirez <andres200493@gmail.com>
2021-11-27 13:21:51 -0600

    S4 - E1

---
+++ s4/server.js
@@ -0,0 +1,62 @@
+// Para el reto
+// const operador = process.argv[2]
+// const num1 = Number(process.argv[3])
+// const num2 = Number(process.argv[4])
+
+// switch (operador) {
+//   case '+':
+//     console.log(num1, operador, num2, "=", num1 + num2);
+//     break;
+//   case '-':
+//     console.log(num1, operador, num2, "=", num1 - num2);
+//     break;
+//   case 'x':
+//     console.log(num1, operador, num2, "=", num1 * num2);
+//     break;
+//   case '/':
+//     console.log(num1, operador, num2, "=", num1 / num2);
+//     break;
+//   default:
+//     console.log("Error");
+//     break;
+// }
+
+////////////////////////////
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+// readline.question('¿Quién eres?', name => {
+//   console.log('Hola', name);
+// })
+
+const question = () => (
+  new Promise((resolve, reject) => {
+    readline.question('¿Quién eres?', name => {
+      console.log('Hola', name);
+      resolve()
+    })
+  })
+)
+
+const question1 = () => (
+  new Promise((resolve, reject) => {
+    readline.question('¿Qué quieres hacer?', name => {
+      console.log('Hola', name);
+      resolve()
+    })
+  })
+)
+
+const main = async () => {
+  await question()
+  await question1()
+}
+
+main()
+
+// readline.question('¿Qué quieres hacer?', name => {
+//   console.log('Hola', name);
+// })
