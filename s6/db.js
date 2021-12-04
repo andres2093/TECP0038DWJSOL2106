df5355bdece5bf70a356204cf935bd5571e1bc6e
Andres Ramirez <andres200493@gmail.com>
2021-12-04 13:13:41 -0600

    S6

---
+++ s6/db.js
@@ -0,0 +1,30 @@
+const { Sequelize } = require('sequelize');
+
+// Importing models
+const Product = require('./models/Product');
+const Review = require('./models/Review');
+
+// Database connection
+const sequelize = new Sequelize('andres', 'admin', 'gYGRQ0Kr', {
+  host: 'mysql-60632-0.cloudclusters.net',
+  dialect: 'mysql',
+  port: 11198,
+  logging: false,
+});
+
+// Getting models
+const models = [
+  Product,
+  Review,
+];
+
+// Registering models into Sequelize
+for (let model of models) {
+  model(sequelize);
+}
+
+// Configuring relations
+const { products, reviews } = sequelize.models;
+reviews.belongsTo(products); // Relation one-to-one in reviews table
+
+module.exports = sequelize;
