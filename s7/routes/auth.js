5959e3daa196ca6a536321553df95cb52d16f0e5
Andres Ramirez <andres200493@gmail.com>
2021-12-18 10:41:30 -0600

    S7 - R1

--- s7/routes/auth.js
+++ s7/routes/auth.js
@@ -27,8 +27,28 @@ router.post('/login', async (req, res) => {
   })
 })
 
-router.post('/signup', (req, res) => {
-  
+router.post('/signup', async (req, res) => {
+  const { body } = req
+  const user = await sequelize.models.users.findOne({ where: {
+    email: body.email
+  }})
+
+  if(user){
+    return res.status(401).json({ message: 'this email is already registered!' })
+  }
+
+  const users = await sequelize.models.users.create({
+    name: body.name,
+    lastname: body.lastname,
+    email: body.email,
+    password: body.password,
+    type: 'client'
+  });
+  await users.save();
+  return res.status(201).json({ 
+    data: users, 
+    message: 'Created!'
+  })
 })
 
 module.exports = router
